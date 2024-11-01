4f261a2c834de014c114f5cab98f5ce9c8d64204
KRISHA ARYA <117719872+Aryagithubk@users.noreply.github.com>
2024-11-01 17:08:54 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (62.90%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -14,13 +14,28 @@ public:
         if(nums[n-1] != nums[n-2]){
             return nums[n-1];
         }
-
-        for(int i=0; i<n-1; i+=2){
-            if(nums[i] != nums[i+1]){
-                ans = nums[i];
-                break;
-            }
+        if(nums[0] != nums[1]){
+            return nums[0];
         }
+
+       int low = 1;
+       int high = n-2;
+
+       while(low <= high){
+        int mid = low + (high-low)/2;
+
+        if(nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]){
+            return nums[mid];
+        }
+        if(mid%2 != 0 and nums[mid] == nums[mid+1]){
+            high = mid-1;
+        }
+        else if(mid%2 == 0 and nums[mid] == nums[mid-1]){
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+     }
 
         return ans;
     }
