e5c4acf213d9f3b017b54c1759c8d90892f99a20
KRISHA ARYA <117719872+Aryagithubk@users.noreply.github.com>
2024-11-09 00:49:49 +0530

    Time: 4 ms (57.14%) | Memory: 99.8 MB (16.72%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
@@ -19,7 +19,8 @@ public:
         // Now process each query from last to first
         for(int i = 0; i < n; i++) {
             result.push_back(currentXOR ^ maxVal);  // Maximum XOR for the current query
-            currentXOR ^= nums[n - 1 - i];          // Remove the last element from XOR
+            currentXOR ^= nums[n - 1 - i];          // Remove the last element from XOR kyuki 
+            // agar last element ko firse xor kr denge toh last xor last zero ho jayega
         }
         
         return result;
