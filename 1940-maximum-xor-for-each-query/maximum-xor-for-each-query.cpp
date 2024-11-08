12030b619d0b6f05fcaad32566f6a681750069c7
KRISHA ARYA <117719872+Aryagithubk@users.noreply.github.com>
2024-11-09 00:05:51 +0530

    Time: 7 ms (34.29%) | Memory: 99.7 MB (27.33%) - LeetSync

---
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        
+        vector<int> result;
+        int n = nums.size();
+        
+        // XOR of all elements in nums
+        int currentXOR = 0;
+        
+        // Compute the XOR of all numbers first
+        for(int i = 0; i < n; i++) {
+            currentXOR ^= nums[i];
+        }
+        
+        // Max value with maximumBit bits is (2^maximumBit - 1)
+        int maxVal = (1 << maximumBit) - 1;
+        
+        // Now process each query from last to first
+        for(int i = 0; i < n; i++) {
+            result.push_back(currentXOR ^ maxVal);  // Maximum XOR for the current query
+            currentXOR ^= nums[n - 1 - i];          // Remove the last element from XOR
+        }
+        
+        return result;
+    }
+};
