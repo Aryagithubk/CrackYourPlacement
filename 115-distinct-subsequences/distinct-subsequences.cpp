17bc9db284a769d597512592b882e04855ee269c
KRISHA ARYA <117719872+Aryagithubk@users.noreply.github.com>
2025-01-30 18:11:46 +0530

    Time: 31 ms (55.28%) | Memory: 44.3 MB (32.80%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    int fxn(int i,int j,string &s,string &t,vector<vector<int>>&dp){
-        //base case
-        //if j went to negative 
-        //means once each char has been traversed
-        if(j < 0){
-            return 1;
-        }
-        //if i went to negative and j still pointing some char 
-        if(i < 0){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        if(s[i] == t[j]){
-            return dp[i][j] = fxn(i-1,j-1,s,t,dp) + fxn(i-1,j,s,t,dp);//adding cond of matching chr then consider it or matching but dont want to consider it ..adding both cond;
-        }
-        return dp[i][j] = fxn(i-1,j,s,t,dp);
-    }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
 
-        return fxn(n-1,m-1,s,t,dp);
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+
+        for(int i=0; i<n; i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return (int)dp[n][m];
     }
 };
