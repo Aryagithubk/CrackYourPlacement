87b41be9c60bfac3e4a61b9562238bf367516198
KRISHA ARYA <117719872+Aryagithubk@users.noreply.github.com>
2025-04-03 02:02:43 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (11.14%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -2,16 +2,38 @@ class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
-    long long maxVal = 0; // We initialize with 0 since we return 0 if all values are negative.
+        if(n < 3) return 0;
 
-    for (int i = 0; i < n - 2; i++) {   // First index
-        for (int j = i + 1; j < n - 1; j++) {  // Second index
-            for (int k = j + 1; k < n; k++) {  // Third index
-                long long value = (long long)(nums[i] - nums[j]) * nums[k];
-                maxVal = max(maxVal, value);
+        vector<int>maxLeft(n,0);//to store max ele of left side of nums[j]
+
+        maxLeft[0] = nums[0];
+        for(int i=1; i<n; i++){
+            maxLeft[i] = max(maxLeft[i-1],nums[i]);
+        }
+
+        vector<int>maxRight(n,0);//to store max ele of right side of nums[j]
+
+        maxRight[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            maxRight[i] = max(maxRight[i+1],nums[i]);
+        }
+
+        //ab hum nums[j] ke sath left aur right ko use krke dekhenge max val
+
+        long long maxi = 0;
+
+        for(int j=1; j<n-1; j++){
+            long long left = maxLeft[j-1];
+            long long right = maxRight[j+1];
+
+            if(left > nums[j]){
+                long long val = (left - nums[j]) * right;
+                maxi = max(maxi,val);
+
             }
         }
-    }
-    return maxVal;
+
+        return maxi;
+
     }
 };
