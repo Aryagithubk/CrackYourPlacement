626b798c81fc21fc1c886d16e19841c5bf6d6641
KRISHA ARYA <117719872+Aryagithubk@users.noreply.github.com>
2025-05-28 06:55:54 +0530

    Time: 81 ms (5.07%) | Memory: 67.3 MB (5.02%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,35 +1,47 @@
 class Solution {
 public:
+    // Top-down DP to compute length
+    int findsubseq(int ind, int prev_ind, vector<int>& nums, int n, vector<vector<int>>& dp) {
+        if (ind == n) return 0;
+
+        if (dp[ind][prev_ind + 1] != -1)
+            return dp[ind][prev_ind + 1];
+
+        int nottaken = findsubseq(ind + 1, prev_ind, nums, n, dp);
+        int taken = 0;
+        if (prev_ind == -1 || nums[ind] % nums[prev_ind] == 0) {
+            taken = 1 + findsubseq(ind + 1, ind, nums, n, dp);
+        }
+
+        return dp[ind][prev_ind + 1] = max(nottaken, taken);
+    }
+
+    // Path reconstruction using dp values
+    vector<int> findpath(int ind, int prev_ind, vector<int>& nums, int n, vector<vector<int>>& dp) {
+        vector<int> ans;
+        while (ind < n) {
+            int nottaken = findsubseq(ind + 1, prev_ind, nums, n, dp);
+            int taken = 0;
+            if (prev_ind == -1 || nums[ind] % nums[prev_ind] == 0) {
+                taken = 1 + findsubseq(ind + 1, ind, nums, n, dp);
+            }
+
+            if (taken > nottaken) { // strict > ensures chain validity
+                ans.push_back(nums[ind]);
+                prev_ind = ind;
+            }
+            ind++;
+        }
+        return ans;
+    }
+
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n = nums.size();
-      vector<int>dp(n,-1);
-      vector<int>prev(n,-1);
-      int maxi = 0;
+        vector<int> ans;
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
 
-      sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
 
-      for(int i=1; i<n; i++){
-        for(int j = 0; j<i; j++){
-            if(nums[i] % nums[j] == 0){
-                if(dp[i] < (dp[j] + 1)){
-                    dp[i] = dp[j]  + 1;
-                    prev[i] = j;
-                }
-            }
-            if(dp[i] > dp[maxi]){
-                maxi = i;
-            }
-        }
-      }
-
-      vector<int>v;
-      while(maxi != -1){
-        v.push_back(nums[maxi]);
-        maxi = prev[maxi];
-      }
-
-      reverse(v.begin(),v.end());
-      return v;
-
+        return findpath(0, -1, nums, n, dp);
     }
 };
