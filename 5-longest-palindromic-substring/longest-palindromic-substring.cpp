b6dfaeefed5cf7d3254c153dfe6df2ccabe675df
KRISHA ARYA <117719872+Aryagithubk@users.noreply.github.com>
2025-06-02 16:12:57 +0530

    Time: 546 ms (14.02%) | Memory: 13.6 MB (59.61%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
+    bool ispalin(string &s, int left, int right){
+        while(left < right){
+            if(s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
     string longestPalindrome(string s) {
         int n = s.length();
-
-        if(n == 0){
-            return "";
+        if(n < 2){
+            return s;
         }
-        int maxi = 1,start = 0;
-        vector<vector<bool>>dp(n,vector<bool>(n,false));
 
-        //for 1 length
+        string maxi = "";
+
         for(int i=0; i<n; i++){
-            dp[i][i] = true;
-        }
-
-        //for 2 length
-        for(int i=0; i<n-1; i++){
-            if(s[i] == s[i+1]){
-                dp[i][i+1] = true;
-                start = i;
-                maxi = 2;
-            }
-        }
-
-        //for 3 or more than
-
-        for(int len=3; len<=n; len++){
-            for(int i=0; i<=n-len; i++){
-                int j = i + len -1;
-
-                if(s[i] == s[j] && dp[i+1][j-1]){
-                    dp[i][j] = true;
-
-                    if(len > maxi){
-                        start = i;
-                        maxi = len;
+            for(int j=i; j<n; j++){
+                if(s[i] == s[j] and ispalin(s,i,j)){
+                    int len = j-i+1;
+                    if(len > maxi.size()){
+                        maxi = s.substr(i,len);
                     }
+
                 }
             }
         }
 
-        return s.substr(start,maxi);
+        return maxi;
+
     }
 };
