9b44fd7acc4a0daf1e3ed68c02720873bbab9ae8
KRISHA ARYA <117719872+Aryagithubk@users.noreply.github.com>
2025-06-30 11:49:18 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (17.42%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int s = 0,e = n-1;
 
-        int x = -1,y=-1;
+        int start = 0;
+        int end = nums.size()-1;
+        int i=  -1,j = -1;
 
-        while(s <= e){
-            int m = s + (e-s)/2;
-            if(target == nums[m]){
-                x = m;
-                e = m-1;
-            }else if(target < nums[m]){
-                e = m-1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target ){
+               i = mid;
+               end = mid-1;//lower bound kr re end ko aage le ja re taaki aur aage ka elements dekhe jo present ho , just way of finding very first index
+            }else if(nums[mid] > target){
+                end = mid-1;
             }else{
-                s = m+1;
+                start = mid+1;
             }
         }
 
-        s = 0,e = n-1;
+        start = 0,end = nums.size()-1;
 
-        while(s <= e){
-            int m = s + (e-s)/2;
-            if(target == nums[m]){
-                y = m;
-                s = m+1;
-            }else if(target < nums[m]){
-                e = m-1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                j = mid;
+                start = mid + 1;//start ko piche le jaa re taaki very last index khoj ske isiliye boundary aage se km kr re aur piche dekh re bs
+
+            }else if(nums[mid] > target){
+                end = mid-1;
             }else{
-                s = m+1;
+                start = mid+1;
             }
         }
 
-        return {x,y};
+        return {i,j};
+
     }
 };
