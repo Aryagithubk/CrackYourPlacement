46178cef750e47a1a9a3aed1eaa5362eeb0aa7fb
KRISHA ARYA <117719872+Aryagithubk@users.noreply.github.com>
2025-07-17 11:20:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (23.47%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-
-    int fxn(vector<int>&nums,int val){
-        int n = nums.size();
-        int arr = 0;
+    int helper(vector<int>&nums, int val){
+        int count = 0;
         int value = 0;
-
-        for(int i=0; i<n; i++){
+        for(int i=0; i<nums.size(); i++){
             if(value + nums[i] <= val){
                 value += nums[i];
             }else{
-                arr++;
+                count++;
                 value = nums[i];
             }
         }
-        return arr;
+
+        return count;
     }
     int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
+        int left = *max_element(nums.begin(),nums.end());
+        int right = accumulate(nums.begin(),nums.end(),0);
 
-        int low = *max_element(nums.begin(),nums.end());
-        int high = accumulate(nums.begin(),nums.end(),0);
+        while(left <= right){
+            int mid = left + (right - left)/2;
 
-        while(low <= high){
-            int mid = low + (high-low)/2;
-
-            if(fxn(nums,mid) >= k){
-                low = mid+1;
+            if(helper(nums,mid) >= k){//means mid ka value chota hai jisse jyada split ho re 
+                left = mid+1;//mid ka value bdha denge
             }else{
-                high = mid-1;
+                right = mid-1;//mid ka value ghata denge
             }
         }
 
-        return low;
+        return left;
     }
 };
